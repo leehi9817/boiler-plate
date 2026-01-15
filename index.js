8540a8142aee054fb29fb32bb8793aa3da1529a2
leehi9817 <leehi9817@gmail.com>
2026-01-15 16:42:06 +0900

    feat: 회원 가입 기능

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const express = require("express");
 const app = express();
 const port = process.env.SERVER_PORT;
 
+// application/x-www-form-urlencoded
+const bodyParser = require("body-parser");
+
+// application/json
+const { User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
 const mongoose = require("mongoose");
 mongoose
   .connect(process.env.DB_DRIVER)
@@ -13,6 +22,20 @@ mongoose
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.post("/register", async (req, res) => {
+  const user = new User(req.body);
+  const result = await user
+    .save()
+    .then(() => {
+      res.status(200).json({
+        success: true,
+      });
+    })
+    .catch((err) => {
+      res.json({ success: false, err });
+    });
 });
 
 app.listen(port, () => {
