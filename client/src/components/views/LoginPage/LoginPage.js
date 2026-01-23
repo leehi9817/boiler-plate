548530934e5de0eb1bf8f86451fb8dcbef0ee7b8
leehi9817 <leehi9817@gmail.com>
2026-01-23 14:19:26 +0900

    style: LoginPage 샘플 레이아웃

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,7 +1,26 @@
 import React from "react";
 
 function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <form style={{ display: "flex", flexDirection: "column" }}>
+        <label>Email</label>
+        <input type="email" value onChange />
+        <label>Password</label>
+        <input type="password" value onChange />
+        <br />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 }
 
 export default LoginPage;
