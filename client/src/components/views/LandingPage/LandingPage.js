761df3fe380aaf07e9c2d0ca355de9e9a7dda5d7
leehi9817 <leehi9817@gmail.com>
2026-01-27 11:55:19 +0900

    feat: 로그아웃 기능 구현

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,10 +1,24 @@
 import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 
 function LandingPage() {
+  const navigate = useNavigate();
+
   useEffect(() => {
     axios.get("/api/hello").then((response) => console.log(response.data));
   }, []);
+
+  const onClickHandler = () => {
+    axios.get("/api/users/logout").then((response) => {
+      if (response.data.success) {
+        navigate("/");
+      } else {
+        alert("로그아웃에 실패하였습니다.");
+      }
+    });
+  };
+
   return (
     <div
       style={{
@@ -16,6 +30,7 @@ function LandingPage() {
       }}
     >
       <h2>시작 페이지</h2>
+      <button onClick={onClickHandler}>Logout</button>
     </div>
   );
 }
